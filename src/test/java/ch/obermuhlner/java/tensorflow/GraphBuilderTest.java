4f14eaa6aafaa5ca6826083345ccc21a6311b168
OBE (Eric Obermühlner) <eric.obermuehlner@profidatagroup.com>
2018-04-24 16:07:15 +0200

    refactor generic test method into assertGraphBuilder()

--- src/test/java/ch/obermuhlner/java/tensorflow/GraphBuilderTest.java
+++ src/test/java/ch/obermuhlner/java/tensorflow/GraphBuilderTest.java
@@ -1,11 +1,12 @@
 package ch.obermuhlner.java.tensorflow;
 
-import ch.obermuhlner.java.tensorflow.GraphBuilder;
 import org.junit.Test;
 import org.tensorflow.Graph;
 import org.tensorflow.Output;
 import org.tensorflow.Session;
 import org.tensorflow.Tensor;
+
+import java.util.function.Function;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -13,20 +14,42 @@ public class GraphBuilderTest {
 
 	@Test
 	public void testConstant () {
+		assertGraphBuilder(2, g -> g.constant("A", 2));
+	}
+
+	private <T> void assertGraphBuilder(T expected, Function<GraphBuilder, Output<T>> graphBuilderFunction) {
 		try (Graph g = new Graph()) {
 			GraphBuilder builder = new GraphBuilder(g);
 
-			Output<Integer> out = builder.constant("A", 2);
+			Output<T> out = graphBuilderFunction.apply(builder);
+			Class<T> type = (Class<T>)expected.getClass();
 
 			try (Session s = new Session(g);
-				Tensor output = s.runner()
+				Tensor<T> output = s.runner()
 					.fetch(out.op().name())
 					.run()
 					.get(0)
-					.expect(Integer.class)) {
-				assertEquals(2, output.intValue());
+					.expect(type)) {
+				T actual = toScalar(output, type);
+				assertEquals(expected, actual);
 			}
 		}
+	}
+
+	private <T> T toScalar (Tensor<T> tensor, Class<T> type) {
+		if (type == Integer.class) {
+			return (T) Integer.valueOf(tensor.intValue());
+		}
+		if (type == Long.class) {
+			return (T) Long.valueOf(tensor.longValue());
+		}
+		if (type == Double.class) {
+			return (T) Double.valueOf(tensor.doubleValue());
+		}
+		if (type == Float.class) {
+			return (T) Float.valueOf(tensor.floatValue());
+		}
+		throw new IllegalArgumentException("Not supported scalar type for tensor: " + type);
 	}
 
 }
